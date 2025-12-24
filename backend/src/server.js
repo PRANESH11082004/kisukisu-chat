872c24d0b135a125446d8b1a12e183615a1d7342
PRANESH11082004 <praneshnarayanan07@gmail.com>
2025-12-24 18:03:27 +0530

    ready for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import Authroutes from './routes/auth.route.js';
-const app = express();
+import path from 'path';
+
 dotenv.config();
+
+const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth",Authroutes);
+
+// make deployment ready
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
 app.listen(PORT,()=>{
     console.log("Server is running on port "+PORT);
 });
 
-app.use("/api/auth",Authroutes);
+
+
+
+
+
 
