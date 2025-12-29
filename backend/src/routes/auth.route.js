a65c41e4fec280f060168b7aee51e9b892c27c99
PRANESH11082004 <praneshnarayanan07@gmail.com>
2025-12-29 11:58:04 +0530

    ready for deployment v1

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,14 +2,14 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/login",(req,res) => {
-    res.send("kisukisuku readiya!!! ");
+    res.send("kisukisuku app ku login page!!! ");
 })
 
 router.get("/signup",(req,res) => {
-    res.send("kisukisuku readiya!!! ");
+    res.send("kisukisuku app ku signup page ready !!! ");
 })
 router.get("/logout",(req,res) => {
-    res.send("kisukisuku! readiya!!! ");
+    res.send("kisukisuku logout !!! ");
 })
 
 export default router;
